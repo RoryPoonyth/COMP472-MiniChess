644a016637319fbbf2bf47b30097a9d7327c556c
OmarEduS <oesl96@gmail.com>
2025-01-19 00:08:48 -0500

    Fixed some issues

--- CODE/MiniChessSkeletonCode.py
+++ CODE/MiniChessSkeletonCode.py
@@ -24,7 +24,6 @@ class MiniChess:
                 ['.', 'wp', 'wp', '.', '.'],
                 ['.', 'wN', 'wB', 'wQ', 'wK']],
                 "turn": 'white',
-                "turnNumber": 1,
                 }
         return state
 
@@ -264,8 +263,6 @@ class MiniChess:
         game_state["board"][start_row][start_col] = '.'
         game_state["board"][end_row][end_col] = piece
         game_state["turn"] = "black" if game_state["turn"] == "white" else "white"
-        #Added to increase turns
-        game_state["turnNumber"] += 1 if game_state["turn"] == "white" else 0
         
         return game_state
 
@@ -274,7 +271,7 @@ class MiniChess:
     @Omar
     """
 
-    def game_over(self, maxTurns, game_state):
+    def game_over(self, game_state):
         # Game Over Knight Captured
         kings = [
             piece
@@ -285,19 +282,19 @@ class MiniChess:
         
         if(len(kings) == 1 ):
             if 'wK' in kings:
-                print("Black Wins") 
-            else:
                 print("White Wins") 
+            else:
+                print("Black Wins") 
             exit(1)
         
-        # Game Over Maximum Turns    
+        # Game Over Maximum Turns Draw 
+        """
+        Todo
         if(game_state["turnNumber"] > int(maxTurns) ):
             print(f"Draw Maximum Turns ({maxTurns}) Reached ") 
             exit(1)
-
+        """ 
         
-        # Game Over Draw
-
     
 
     """
@@ -327,13 +324,11 @@ class MiniChess:
     """
     def play(self):
         print("Welcome to Mini Chess! Enter moves as 'B2 B3'. Type 'exit' to quit.")
-        maxTurns = input("Please select the maximum number of turns for the match: ")
 
         while True:
             self.display_board(self.current_game_state)
-            self.game_over(maxTurns, self.current_game_state)
+            self.game_over(self.current_game_state)
 
-            print(f"Turn Number: {self.current_game_state['turnNumber']}")
             move = input(f"{self.current_game_state['turn'].capitalize()} to move: ")
             if move.lower() == 'exit':
                 print("Game exited.")
